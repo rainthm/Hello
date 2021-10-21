7a9ab3a8ba79f2375ed9a318c3085c7122a695d9
rainthm <rainthm@139.com>
2021-10-21 11:09:02 +0800

    finished exercise 15-5

--- chapter15/random_walk.py
+++ chapter15/random_walk.py
@@ -11,13 +11,16 @@ class RandomWalk():
 
     def fill_walk(self):
         while len(self.x_v) < self.num_points:
-            x_direction = choice([1, -1])
-            y_direction = choice([1, -1])
-            x_distance = choice([0,1,2,3,4])
-            y_distance = choice([0,1,2,3,4])
-            x_step = x_direction * x_distance
-            y_step = y_direction * y_distance
+            # x_direction = choice([1, -1])
+            # y_direction = choice([1,-1])
+            # x_distance = choice([0,1,2,3,4])
+            # y_distance = choice([0,1,2,3,4])
+            # x_step = x_direction * x_distance
+            # y_step = y_direction * y_distance
 
+            x_step = self.get_step()
+            y_step = self.get_step()
+            
             if x_step == 0 and y_step == 0:
                 continue
 
@@ -26,3 +29,9 @@ class RandomWalk():
 
             self.x_v.append(next_x)
             self.y_v.append(next_y)
+
+    def get_step(self):
+        direction = choice([1,-1])
+        distance = choice([0,1,2,3,4])
+        return direction * distance
+            
